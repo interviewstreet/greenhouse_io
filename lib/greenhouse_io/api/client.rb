4577621bef1af780d7cc7fb2ca1980a33ec30255
Mayank Nader <mayank.nader11@gmail.com>
2022-09-22 19:36:48 +0530

    permit starts_after, starts_before and status as query params

--- lib/greenhouse_io/api/client.rb
+++ lib/greenhouse_io/api/client.rb
@@ -3,7 +3,7 @@ module GreenhouseIo
     include HTTMultiParty
     include GreenhouseIo::API
 
-    PERMITTED_OPTIONS = [:page, :per_page, :job_id]
+    PERMITTED_OPTIONS = [:page, :per_page, :job_id, :starts_after, :starts_before, :status]
 
     attr_accessor :api_token, :rate_limit, :rate_limit_remaining, :link
     base_uri 'https://harvest.greenhouse.io/v1'
